c18987400da7e38e7b4adce893d6b96aa5c4ffa9
adetiamarhadi <adetia.marhadi@gmail.com>
2022-04-16 03:46:26 +0700

    update func main

--- src/hex/cmd/main.go
+++ src/hex/cmd/main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
 
+	"github.com/adetiamarhadi/golang-hex-arch/internal/adapters/app/api"
 	"github.com/adetiamarhadi/golang-hex-arch/internal/adapters/core/arithmetic"
+	"github.com/adetiamarhadi/golang-hex-arch/internal/adapters/framework/right/db"
+	"github.com/adetiamarhadi/golang-hex-arch/internal/ports"
+
+	gRPC "github.com/adetiamarhadi/golang-hex-arch/internal/adapters/framework/left/grpc"
 )
 
 func main() {
-	
-	arithmetic := arithmetic.NewAdapter()
-	result, err := arithmetic.Add(1, 3)
-	if err == nil {
-		fmt.Println(result)
+
+	var err error
+
+	var dbaseAdapter ports.DbPort
+	var core ports.ArithmeticPort
+	var appAdapter ports.APIPort
+	var gRPCAdapter ports.GRPCPort
+
+	dbaseDriver := os.Getenv("DB_DRIVER")
+	dsourceName := os.Getenv("DS_NAME")
+
+	dbaseAdapter, err = db.NewAdapter(dbaseDriver, dsourceName)
+	if err != nil {
+		log.Fatalf("Failed to initiate dbase connection: %v", err)
 	}
+	defer dbaseAdapter.CloseDbConnection()
+
+	core = arithmetic.NewAdapter()
+
+	appAdapter = api.NewAdapter(dbaseAdapter, core)
+
+	gRPCAdapter = gRPC.NewAdapter(appAdapter)
+
+	gRPCAdapter.Run()
 }
